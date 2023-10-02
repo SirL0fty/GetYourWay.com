dbddb61dc562a382905df12abf12b40456e18436
adamSH91 <adamserraj91@gmail.com>
2023-10-02 20:54:28 +0100

    Added initial Spring Boot files

---
+++ GetYourWay/src/main/java/uk/sky/jkamp/GetYourWay/GetYourWayApplication.java
@@ -0,0 +1,13 @@
+package uk.sky.jkamp.GetYourWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetYourWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetYourWayApplication.class, args);
+	}
+
+}
---
+++ GetYourWay/src/test/java/uk/sky/jkamp/GetYourWay/GetYourWayApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.sky.jkamp.GetYourWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetYourWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
