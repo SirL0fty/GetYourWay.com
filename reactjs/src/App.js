768bca51758fd9c00a6e1d4cc119907145efbd45
JulieKellachan <julie.kellachan@gmail.com>
2023-10-03 19:01:39 +0100

    adding reactjs

---
+++ reactjs/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          <h1>GetYourWay.com</h1>
+          <p>Coming soon...</p>
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
