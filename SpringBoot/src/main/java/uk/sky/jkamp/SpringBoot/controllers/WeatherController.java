b1affe177e3b0e8c8fc0668e5b4ad5a5e61d3304
adamSH91 <adamserraj91@gmail.com>
2023-10-08 22:07:42 +0100

    added get weather by city api

--- SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/controllers/WeatherController.java
+++ SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/controllers/WeatherController.java
@@ -1,4 +1,25 @@
 package uk.sky.jkamp.SpringBoot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uk.sky.jkamp.SpringBoot.services.WeatherService;
+
+@RestController
 public class WeatherController {
+    private WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    /**
+     * An API that retrieves the current weather conditions for a city
+     * @param city - The name of the city
+     * @return - A JSON object in String format with detailed results of the weather
+     */
+    @GetMapping("/getCurrentWeatherByCity")
+    public String getCurrentWeatherByCity(@RequestParam(value = "city") String city) {
+        return this.weatherService.getCurrentWeatherByCity(city);
+    }
 }
--- SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/services/WeatherService.java
+++ SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/services/WeatherService.java
@@ -1,4 +1,27 @@
 package uk.sky.jkamp.SpringBoot.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class WeatherService {
+    @Value("${openWeatherApiKey}")
+    private String openWeatherApiKey;
+
+    public WeatherService() {}
+
+    /**
+     * Retrieves the current weather conditions for a city
+     * @param city - The name of the city
+     * @return - A JSON object in String format with detailed results of the weather
+     */
+    public String getCurrentWeatherByCity(String city) {
+        WebClient webClient = WebClient.create("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + openWeatherApiKey);
+
+        return webClient.get()
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
