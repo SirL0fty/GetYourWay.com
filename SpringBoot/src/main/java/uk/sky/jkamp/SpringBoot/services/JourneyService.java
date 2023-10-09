f4343306f7028a3420351c4c0fc8fa848a41b763
Preetirachel <preetirachel.official@gmail.com>
2023-10-09 17:26:06 +0100

    services update- address, journey and location

---
+++ SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/services/JourneyService.java
@@ -0,0 +1,21 @@
+package uk.sky.jkamp.SpringBoot.services;
+
+import jakarta.validation.Valid;
+import org.springframework.data.jpa.repository.Query;
+import uk.sky.jkamp.SpringBoot.entities.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class JourneyService {
+
+
+    private JourneyRepo journeyRepo;
+    private List<Journey> journeys = new ArrayList<>();
+
+    public List<Journey> findAllJourneys() {
+        return this.journeyRepo.findAllJourney();
+    }
+}
