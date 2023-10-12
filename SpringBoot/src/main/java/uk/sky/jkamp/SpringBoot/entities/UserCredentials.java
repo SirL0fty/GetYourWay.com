77846a665c702d5c54919308350ad0c344d27735
Preetirachel <preetirachel.official@gmail.com>
2023-10-12 11:08:02 +0100

    Filtering for Programme and tests on Postman done

---
+++ SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/entities/UserCredentials.java
@@ -0,0 +1,23 @@
+package uk.sky.jkamp.SpringBoot.entities;
+
+public class UserCredentials {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordc() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Login />
       <Footer />
     </div>
+
   );
 }
 
