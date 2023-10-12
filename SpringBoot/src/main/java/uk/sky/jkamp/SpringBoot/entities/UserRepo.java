bdf5d2925c336d7ed20dc663d96a0960cbcdc410
Preetirachel <preetirachel.official@gmail.com>
2023-10-12 17:27:49 +0100

    Filtering for Programme and tests on Postman done

--- SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/entities/UserRepo.java
+++ SpringBoot/src/main/java/uk/sky/jkamp/SpringBoot/entities/UserRepo.java
@@ -5,9 +5,10 @@ package uk.sky.jkamp.SpringBoot.entities;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import uk.sky.jkamp.SpringBoot.entities.User;
 
-
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String userName);
