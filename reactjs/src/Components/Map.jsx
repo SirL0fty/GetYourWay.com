a59f750c7e68205ff76b05260560ee258c1e9c81
adamSH91 <71341031+adamSH91@users.noreply.github.com>
2023-10-12 10:06:27 +0100

    implemented obtaining user live location if permitted

--- reactjs/src/Components/Map.jsx
+++ reactjs/src/Components/Map.jsx
@@ -8,9 +8,14 @@ const Map = () => {
 
   const mapContainer = useRef(null);
   const map = useRef(null);
+  //longitude and latitude for the center of the map
   const [lng, setLng] = useState(-5.0315);
   const [lat, setLat] = useState(48.5841);
   const [zoom, setZoom] = useState(3.2);
+
+  // State for user coordinates is set to invalid coordinates as initial values
+  const [userLongitude, setUserLongitude] = useState(999.00);
+  const [userLatitude, setUserLatitude] = useState(999.00);
 
   const geojson = {
     type: 'FeatureCollection',
@@ -49,6 +54,22 @@ const Map = () => {
       zoom: zoom
     });
 
+    // Obtains the user's current location if the user has allowed it
+    let geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true
+    })
+
+    map.current.addControl(geolocate);
+
+    geolocate.on("geolocate", (e) => {
+      setUserLongitude(e.coords.longitude.toFixed(4));
+      setUserLatitude(e.coords.latitude.toFixed(4));
+    });
+
     map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
@@ -74,9 +95,6 @@ const Map = () => {
 
   return (
     <>
-      <div>
-        Latitude: {lat}, Longitude: {lng}
-      </div>
       <div ref={mapContainer} className='map-container' />
     </>
   )
