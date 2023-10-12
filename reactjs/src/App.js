30dff5f255dd044052c09652aec7f55dc136421c
adamSH91 <71341031+adamSH91@users.noreply.github.com>
2023-10-12 13:26:16 +0100

    fixed authorization and login component

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,18 +1,26 @@
 
 import "./App.css";
+import { useState } from 'react';
 import Header from "./Components/Header";
 import Login from "./Components/Login";
 import Footer from "./Components/Footer";
-import FlightSearch from "./Components/FlightSearch";
+//import FlightSearch from "./Components/FlightSearch";
 import Map from './Components/Map';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
       <Header />
-      <Login />
-      <Map />
-      <FlightSearch />
+      {!loggedIn && 
+        <Login setLoggedIn={setLoggedIn} />
+      }
+
+      {loggedIn &&
+        <Map />
+      }
+      {/* <FlightSearch /> */}
       <Footer />
     </div>
 
